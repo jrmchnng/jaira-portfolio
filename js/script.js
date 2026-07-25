6523d53e293babcc6e0082ab8339ee250ef1b0dd
Jaira <jairanunag.asi@gmail.com>
2026-07-25 09:42:34 +0800

    added scroll to top button

--- js/script.js
+++ js/script.js
@@ -229,3 +229,38 @@ window.addEventListener("resize", () => {
         closeMobileMenu();
     }
 });
+
+/*==========================================
+BACK TO TOP BUTTON
+==========================================*/
+
+const backToTop =
+    document.getElementById("backToTop");
+
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 350) {
+
+        backToTop.classList.add("show");
+
+    } else {
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
